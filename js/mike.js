1607e59a5036d1633550ac9d83fb4871cfcaebf2
Mike <mikebudahn@live.com>
2016-06-16 12:08:00 -0400

    My JavaScript functions.

---
+++ js/mike.js
@@ -0,0 +1,15 @@
+// MIKE.JS
+// Mike Budahn, 6/2016
+//
+// ON LOAD
+(function() {
+	// Build drop-down navigation menu
+	var menuItems = "/menu/menu-items.js";
+	$.getJSON(menuItems, function(data) {
+		$.each(data, function( i, item ) {
+			$("#myNavigation .dropdown-menu").append("<li><a href='" + item + "'>" + i + "</a></li>");
+		});
+	});
+	//
+
+})();
