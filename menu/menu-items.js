460f42bd913e760fb915f32b41be6bce5efb2bf7
Mike <mikebudahn@live.com>
2016-06-20 20:36:06 -0400

    Added Demo 05.

--- menu/menu-items.js
+++ menu/menu-items.js
@@ -1,5 +1,6 @@
 {
 	"01-Hello World":"index-01-helloworld.html",
 	"03-Directives":"index-03-directives-b.html",
-	"04-Controllers":"index-04-controllers.html"
+	"04-Controllers":"index-04-controllers.html",
+	"05-Routing":"index-05-routing.html"
 }
