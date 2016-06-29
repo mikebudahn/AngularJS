34351d862e5f2accc2cab8bd13c0589904a3e93e
Mike <mikebudahn@live.com>
2016-06-28 20:42:39 -0400

    Customers Factory for index-06.

---
+++ app/services/customersFactory-06.js
@@ -0,0 +1,94 @@
+(function() {
+	
+	// create a factory function with customers
+	var customerFactory = function () {
+		var customers = [
+            {
+                id: 1, 
+                joined: '2000-12-02', 
+                name:'John', 
+                city:'Norfolk', 
+                orderTotal: 9.9956,
+                orders: [
+                    {
+                        id: 1,
+                        product: 'Shoes',
+                        total: 9.9956
+                    }
+                ]
+            }, 
+            {
+                id: 2, 
+                joined: '1965-01-25',
+                name:'Bill', 
+                city:'Chesapeake', 
+                orderTotal: 19.99,
+                orders: [
+                    {
+                        id: 2,
+                        product: 'Baseball',
+                        total: 9.995
+                    },
+                    {
+                        id: 3,
+                        product: 'Bat',
+                        total: 9.995
+                    }
+                ]
+            },
+            {
+                id: 3, 
+                joined: '1944-06-15',
+                name:'Tina', 
+                city:'Hampton', 
+                orderTotal:44.99,
+                orders: [
+                    {
+                        id: 4,
+                        product: 'Headphones',
+                        total: 44.99
+                    }
+                ]
+            }, 
+            {
+                id: 4, 
+                joined: '1995-03-28',
+                name:'Dave', 
+                city:'Yorktown', 
+                orderTotal:101.50,
+                orders: [
+                    {
+                        id: 5,
+                        product: 'Kindle',
+                        total: 101.50
+                    }
+                ]
+            }
+        ];
+
+        // create a factory object
+        var factory = {};
+        // define a function for the factory to perform
+        factory.getCustomers = function() {
+        	return customers;
+        };
+        
+        factory.getCustomer = function(customerId) {
+            //Search the customers for the customerId
+            //Return the customer or null if not found
+            for (var i=0,len=customers.length;i<len;i++) {
+               if (customers[i].id === parseInt(customerId)) {
+                   return customers[i];
+               }
+            }
+            return {};
+        }
+
+        // return the factory
+        return factory;
+	};
+
+	// Register the factory
+	angular.module('customersApp').factory('customersFactory',customerFactory);
+
+}());
