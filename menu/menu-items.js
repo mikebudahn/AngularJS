966e269cdd91f02e9c76769a74621b36f92ffe0d
Mike <mikebudahn@live.com>
2016-07-12 16:19:27 -0400

    Demo 07 Ajax/REST added. Full URL paths added.

--- menu/menu-items.js
+++ menu/menu-items.js
@@ -1,7 +1,8 @@
 {
-	"01-Hello World":"index-01-helloworld.html",
-	"03-Directives":"index-03-directives-b.html",
-	"04-Controllers":"index-04-controllers.html",
-	"05-Routing":"index-05-routing.html",
-	"06-Factories":"index-06-factory.html"
+	"01-Hello World":"http://localhost/index-01-helloworld.html",
+	"03-Directives":"http://localhost/index-03-directives-b.html",
+	"04-Controllers":"http://localhost/index-04-controllers.html",
+	"05-Routing":"http://localhost/index-05-routing.html",
+	"06-Factories":"http://localhost/index-06-factory.html",
+	"07-Ajax/REST":"http://localhost:8080/index-07-ajax.html"
 }
