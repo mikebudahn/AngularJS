c6c39393aaf0fc885fb9c8bc319f8c6da5534140
Mike <mikebudahn@live.com>
2016-07-12 16:10:33 -0400

    Customer Controller for demo 07.

---
+++ app/controllers/customersController-07.js
@@ -0,0 +1,34 @@
+(function() {
+    
+    var CustomersController = function ($scope, customersFactory, appSettings) {
+        $scope.sortBy = 'name';
+        $scope.reverse = false;
+        $scope.customers = [];
+        $scope.appSettings = appSettings;
+
+        function init () {
+            customersFactory.getCustomers()
+                .success(function(customers) {
+                    $scope.customers = customers;
+                })
+                .error(function(data, status, headers, config) {
+                    // handle error
+                });
+        }
+
+        init();
+        
+        $scope.doSort = function(propName) {
+           $scope.sortBy = propName;
+           $scope.reverse = !$scope.reverse;
+        };
+        
+    };
+    
+    // fix minify
+    CustomersController.$inject = ['$scope', 'customersFactory', 'appSettings'];
+
+    angular.module('customersApp')
+      .controller('CustomersController', CustomersController);
+    
+}());
